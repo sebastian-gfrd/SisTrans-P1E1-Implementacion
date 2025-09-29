9569ad97c6ebc33b5bf35c7bec4519810154e04c
sebastian-gfrd <sc.martineza12@uniandes.edu.co>
2025-09-29 17:12:10 -0500

    3 repositorios

--- alpescab/src/main/java/uniandes/edu/co/alpescab/repositorio/CiudadRepository.java
+++ alpescab/src/main/java/uniandes/edu/co/alpescab/repositorio/CiudadRepository.java
@@ -1,7 +1,20 @@
 package uniandes.edu.co.alpescab.repositorio;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
 import uniandes.edu.co.alpescab.modelo.CIUDAD;
 
-public interface CiudadRepository extends JpaRepository<CIUDAD, Integer> {
+
+public interface CiudadRepository extends JpaRepository<CIUDAD,Integer> {
+
+    //RF1
+
+   @Modifying
+   @Transactional
+   @Query(value = "INSERT INTO CIUDAD(NOMBRE, DEPARTAMENTO) VALUES(SaokiBase_sequence.nextval,:NOMBRE, :DEPARTAMENTO)", nativeQuery = true)
+    void insertarCIUDAD(@Param("NOMBRE") String nOMBRE, @Param("DEPARTAMENTO")String dEPARTAMENTO);
 }
