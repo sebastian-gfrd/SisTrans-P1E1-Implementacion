5bc1a8913d29328fd8106bc327e333d3307670b9
sebastian-gfrd <sc.martineza12@uniandes.edu.co>
2025-09-29 19:10:21 -0500

    Arreglo

--- alpescab/src/main/java/uniandes/edu/co/alpescab/repositorio/PersonaRepository.java
+++ alpescab/src/main/java/uniandes/edu/co/alpescab/repositorio/PersonaRepository.java
@@ -3,5 +3,5 @@ package uniandes.edu.co.alpescab.repositorio;
 import org.springframework.data.jpa.repository.JpaRepository;
 import uniandes.edu.co.alpescab.modelo.PERSONA;
 
-public interface PersonaRepository extends JpaRepository<PERSONA, String> {
+public interface PersonaRepository extends JpaRepository<PERSONA, Integer> {
 }
